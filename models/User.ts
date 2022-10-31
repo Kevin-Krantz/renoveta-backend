153a2d2647fd2a8a1dd467652a1f726d9643c4b5
Kevin Krantz <kevinkrantz95@hotmail.com>
2022-10-31 04:21:58 +0100

    Changed name from required to not required. Also changed password minimum length to 5 to match the frontend validation.

--- models/User.ts
+++ models/User.ts
@@ -5,10 +5,10 @@ import mongoose, { Schema, Model } from "mongoose";
 import "mongoose-type-email";
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
-  name: { type: String, minlength: 2, required: true },
+  name: { type: String, minlength: 2 },
   // @ts-ignore
   email: { type: mongoose.SchemaTypes.Email, required: true },
-  password: { type: String, minlength: 5, required: true },
+  password: { type: String, minlength: 6, required: true },
 });
 
 userSchema.methods.generateAuthToken = function () {
@@ -23,9 +23,9 @@ const User: Model<IUser> = mongoose.model("User", userSchema);
 
 const validateUser = (user: IUser) => {
   const schema = Joi.object({
-    name: Joi.string().min(2).required(),
+    name: Joi.string().min(2),
     email: Joi.string().email().required(),
-    password: Joi.string().min(5).required(),
+    password: Joi.string().min(6).required(),
   });
 
   return schema.validate(user);
