5a028735e799fdb2b5b58ddaeec49b458ad3eda4
Kevin Krantz <kevinkrantz95@hotmail.com>
2022-11-12 04:44:34 +0100

    Made enums for roofmaterial

---
+++ types/RoofMaterial.ts
@@ -0,0 +1,8 @@
+export enum RoofMaterial {
+  BRICK = "Brick",
+  SHEETMETAL = "Sheet Metal",
+  ETERNIT = "Eternit",
+  ROOFINGFELT = "Roofing Felt",
+  ROOFSHINGLE = "Roof Shingle",
+  OTHER = "Other",
+}
