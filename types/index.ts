e818fbce9fdb41692e83328b4fef913449f2fbb5
Kevin Krantz <kevinkrantz95@hotmail.com>
2022-11-12 04:35:12 +0100

    made index.ts and put all the interfaces and enums exports here to make the imports in Form.ts look better

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+// Enums
+export { RenovationType } from "types/RenovationType";
+export { TypeOfRoof } from "types/TypeOfRoof";
+export { RoofMaterial } from "types/RoofMaterial";
+
+// Types
+export type {
+  IForm,
+  HouseMeasurements,
+  UserInfo,
+  Residence,
+} from "types/IForm";
+export type { IUser } from "types/IUser";
