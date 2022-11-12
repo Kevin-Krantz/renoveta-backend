71da323ce9266ed7dba3c07cbff14a43e47052bd
Kevin Krantz <kevinkrantz95@hotmail.com>
2022-11-12 04:44:55 +0100

    made enums for typeofroof

---
+++ types/TypeOfRoof.ts
@@ -0,0 +1,9 @@
+export enum TypeOfRoof {
+  GABLEROOF = "Gableroof",
+  SHEDROOF = "Shedroof",
+  HIPROOF = "Hiproof",
+  MANSARDROOF = "Mansardroof",
+  BUTTERFLYROOF = "Butterflyroof",
+  FLATROOF = "Flatroof",
+  OTHER = "Other",
+}
