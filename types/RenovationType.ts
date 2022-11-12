440ce3b56514159f10a6a6d72f6c22257af85793
Kevin Krantz <kevinkrantz95@hotmail.com>
2022-11-12 04:39:46 +0100

    Made enums for renoverings-typ

---
+++ types/RenovationType.ts
@@ -0,0 +1,8 @@
+export enum RenovationType {
+  SHEETMETALWORK = "Sheetmetalwork",
+  REEROOFING = "Reroofing",
+  ROOFRENOVATION = "Roofrenovation",
+  ROOFREPLACEMENT = "Roofreplacement",
+  ROOFWASHING = "Roofwashing",
+  OTHER = "Other",
+}
