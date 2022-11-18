18ffbed6b4290f8a5d88c42ab42a028b1eebee8f
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-11-18 16:56:54 +0100

    fixed get routes for forms

--- routes/form.ts
+++ routes/form.ts
@@ -1,29 +1,38 @@
 import express from "express";
+import admin from "middleware/admin";
 import { User } from "models/User";
+import auth from "../middleware/auth";
 import { validateForm as validate, RenovetaForm } from "../models/Form";
+
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req: any, res) => {
   // bara auth
   // typa upp req
-  const forms = await RenovetaForm.find({ user: (req as any).user });
+  const forms = await RenovetaForm.find({ user: req.user });
+  console.log(req.user);
+
   return res.send(forms);
 });
 
-router.get("/all", async (req, res) => {
+router.get("/all", [auth, admin], async (req: any, res: any) => {
   // auth och admin
+
   const forms = await RenovetaForm.find();
   return res.send(forms);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   // bara auth
-  const forms = await RenovetaForm.find();
-  console.log(forms);
-  return res.send(forms);
+  const form: any = await RenovetaForm.findById(req.params.id);
+
+  if (!form)
+    return res.status(404).send("The form with the given id was not found");
+
+  return res.send(form);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.message);
 
